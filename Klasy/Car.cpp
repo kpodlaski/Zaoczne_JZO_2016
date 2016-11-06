b572e9ff8f437e5221f79291d4be3e6c5f0c5f7d
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2016-11-06 17:05:08 +0100

    Car.cpp created

--- Klasy/Car.cpp
+++ Klasy/Car.cpp
@@ -1,11 +1,24 @@
 #include "stdafx.h"
 #include "Car.h"
+#include "Person.h"
 
+void Car::soldTo(Person person) {
+	owner = person;
+}
+
+Car::Car(Person _owner, int _year, string _brand, string _version) : owner(_owner), year(_year), brand(_brand), version(_version)
+{
+
+}
 
 Car::Car()
 {
 }
 
+Car::Car(const Car & car) : owner(car.owner), brand(car.brand), version(car.version), year(car.year)
+{
+
+}
 
 Car::~Car()
 {
